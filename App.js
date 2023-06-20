3d3e3f9fa6981607e972fd4bebd9bdcbf6f223b0
millerm30 <millergm30@gmail.com>
2023-06-20 14:08:52 -0230

    testing purposes

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const User = (props) => {
+  return (
+    <View>
+      <Text>Hello, I am {props.name}!</Text>
+    </View>
+  );
+}
+
+const Users = () => {
+  return (
+    <View>
+      <Text>Welcome!</Text>
+      <User name={"mike"}/>
+      <User name={"john"}/>
+      <User name={"paul"}/>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Users />
       <StatusBar style="auto" />
     </View>
   );
